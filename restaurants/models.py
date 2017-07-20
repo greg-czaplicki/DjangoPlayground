b5cae3a7be709500fb575f4c7b0acb6fcae48265
glc3344 <glc3344@gmail.com>
2017-07-19 20:23:52 -0400

    Signals and Slug
    Feat: pre-save slug generator

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save
+
+from .utils import unique_slug_generator
 
 
 # Create your models here.
@@ -14,3 +17,12 @@ class RestaurantLocation(models.Model):
     @property
     def title(self):
         return self.name
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    print('saving..')
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
